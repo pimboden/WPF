aa70257e494d3cb126c7a328894d8cb54c863848
Patrick Imboden <pimboden@msn.com>
2018-01-15 17:43:50 +0100

    002 Treeview and Value converters

---
+++ 002_TreeViewAndValueConverters/Helpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeViewAndValueConverters
+{
+    public static class StringExtensions
+    {
+
+        public static string GetFileFolderName(this string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                return string.Empty;
+            var normalizedPath = path.Replace('/',Path.DirectorySeparatorChar);
+            var indexOfSeparator = normalizedPath.LastIndexOf(Path.DirectorySeparatorChar);
+            return indexOfSeparator <= 0 ? path : normalizedPath.Substring(indexOfSeparator + 1);
+        }
+    }
+}
---
+++ 002_TreeViewAndValueConverters/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace TreeViewAndValueConverters
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var logicalDrive in Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem
+                {
+                    Header = logicalDrive,
+                    Tag = logicalDrive
+                };
+                item.Items.Add(null);
+                item.Expanded += Folder_Expanded;
+                FolderView.Items.Add(item);
+            }
+        }
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            var treeViewItem = (TreeViewItem) sender;
+            if(treeViewItem.Items.Count != 1 || treeViewItem.Items[0] != null)
+                return;
+            treeViewItem.Items.Clear();
+            var fullPath = (string)treeViewItem.Tag;
+            var directories = new List<string>();
+            try
+            {
+                var dirs = Directory.GetDirectories(fullPath);
+                directories.AddRange(dirs);
+
+            }
+            catch
+            {
+                
+            }
+            foreach (var directory in directories)
+            {
+                var newTreeViewItem = new TreeViewItem
+                {
+                    Tag = directory,
+                    Header = directory.GetFileFolderName()
+                };
+                newTreeViewItem.Items.Add(null);
+                newTreeViewItem.Expanded += Folder_Expanded;
+                treeViewItem.Items.Add(newTreeViewItem);
+            }
+
+
+            var files = new List<string>();
+            try
+            {
+                var fs = Directory.GetFiles(fullPath);
+                files.AddRange(fs);
+
+            }
+            catch
+            {
+
+            }
+            foreach (var file in files)
+            {
+                var newTreeViewItem = new TreeViewItem
+                {
+                    Tag = file,
+                    Header =file.GetFileFolderName()
+                };
+                treeViewItem.Items.Add(newTreeViewItem);
+            }
+        }
+
+    }
+}
