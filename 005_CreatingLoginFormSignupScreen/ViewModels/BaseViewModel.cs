66248e74228ef5c100f1aa09ec3eb613d6f8024b
Patrick Imboden <pimboden@msn.com>
2018-02-02 14:17:47 +0100

    CreatingLoginFormSignupScreen Ready for start

---
+++ 005_CreatingLoginFormSignupScreen/ViewModels/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace Learn.Wpf.ViewModels
+{
+
+    /// <summary>
+    /// Base viewModel that fires PropertyChanged events as needed
+    /// </summary>
+    public abstract class BaseViewModel: INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The event that is fired when any child property changes its value
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+
+        /// <summary>
+        /// Call this to fire <see cref="PropertyChnaged"/> event
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnPropertyChnaged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name) );
+        }
+    }
+}
