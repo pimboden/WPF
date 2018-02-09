dc7b6c2087810fc7e221153ce1495aaa0d207ae8
Patrick Imboden <pimboden@msn.com>
2018-02-09 17:03:46 +0100

    006_AttachedProperties The real but ugly way

---
+++ 006_AttachedProperties/AttachedProperties/PasswordBoxProperties.cs
@@ -0,0 +1,58 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Learn.Wpf.AttachedProperties
+{
+    public class PasswordBoxProperties
+    {
+
+        public static readonly DependencyProperty MonitorPasswordProperty = DependencyProperty.RegisterAttached("MonitorPassword", typeof(bool), typeof(PasswordBoxProperties), new PropertyMetadata(false, OnMonitorPasswordChanged));
+
+        private static void OnMonitorPasswordChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if(!(d is PasswordBox passwordBox))
+                return;
+            passwordBox.PasswordChanged -= PasswordBox_PasswordChanged;
+
+            if ((bool) e.NewValue)
+            {
+                SetHasText(passwordBox);
+                passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
+            }
+        }
+
+        private static void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is PasswordBox passwordBox))
+                return;
+             SetHasText(passwordBox);
+        }
+        
+
+        public static void SetMonitorPassword(PasswordBox element, bool value)
+        {
+            element.SetValue(MonitorPasswordProperty, value);
+        }
+
+        public static bool GetMonitorPassword(PasswordBox element)
+        {
+            return (bool)element.GetValue(MonitorPasswordProperty);
+        }
+
+
+
+
+        public static readonly DependencyProperty HasTextProperty = DependencyProperty.RegisterAttached("HasText", typeof(bool), typeof(PasswordBoxProperties),new PropertyMetadata(false));
+
+        private static void SetHasText(PasswordBox element)
+        {
+            element.SetValue(HasTextProperty, element.SecurePassword.Length >0);
+        }
+
+        public  static bool GetHasText(PasswordBox element)
+        {
+           return (bool)element.GetValue(HasTextProperty);
+        }
+    }
+}
