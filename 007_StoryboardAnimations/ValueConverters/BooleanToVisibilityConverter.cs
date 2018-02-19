1394f5b1df33619c557f0f991dc08dd473ed5998
Patrick Imboden <pimboden@msn.com>
2018-02-19 13:01:26 +0100

    007_StoryboardAnimations Start

---
+++ 007_StoryboardAnimations/ValueConverters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace Learn.Wpf.ValueConverters
+{
+    /// <summary>
+    /// Changes the   <see cref="Visibility"/> dependant on the boolean value
+    /// </summary>
+    public class BooleanToVisibilityConverter : BaseValueConverter<BooleanToVisibilityConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool) value ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
