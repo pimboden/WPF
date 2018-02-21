299e0b633b8fe3ab588558e081e5cc6973f9d50e
Patrick Imboden <pimboden@msn.com>
2018-02-21 15:04:50 +0100

    ItemsControlChatListAndDesignTimeData Work in progress

---
+++ 010_ItemsControlChatListAndDesignTimeData/ViewModels/Chat/Design/ChatListDesignModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn.Wpf.ViewModels.Chat.Design
+{
+
+    /// <summary>
+    /// design time data for a ChatListDesignModel
+    /// </summary>
+    public class ChatListDesignModel : ChatListViewModel
+    {
+        private static ChatListDesignModel _instance;
+
+        public static ChatListDesignModel Instance => _instance ?? (_instance = new ChatListDesignModel());
+
+        public ChatListDesignModel()
+        {
+            Items = new List<ChatListItemViewModel>
+            {
+                new ChatListItemViewModel{Initials = "AA", Message = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam", Name = "Andres Amstein", ProfilePictureRGB = "ccdd66"},
+                new ChatListItemViewModel{Initials = "BB", Message = "Stet clita kasd gubergren, no sea takimata sanctus est", Name = "Bo Broww", ProfilePictureRGB = "ffdd33"},
+                new ChatListItemViewModel{Initials = "CC", Message = "Duis autem vel eum iriure dolor in hendrerit in vulputate", Name = "Carl Camambert", ProfilePictureRGB = "4578d3"},
+                new ChatListItemViewModel{Initials = "DD", Message = "am liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer", Name = "Dagonert Duck", ProfilePictureRGB = "ddee33"},
+            };
+        }
+    }
+}
---
+++ 010_ItemsControlChatListAndDesignTimeData/ViewModels/Chat/Design/ChatListItemDesignModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn.Wpf.ViewModels.Chat.Design
+{
+
+    /// <summary>
+    /// design time data for a ChatListItemViewModel
+    /// </summary>
+    public class ChatListItemDesignModel : ChatListItemViewModel
+    {
+        private static ChatListItemDesignModel _instance;
+
+        public static ChatListItemDesignModel Instance => _instance ?? (_instance = new ChatListItemDesignModel());
+
+        public ChatListItemDesignModel()
+        {
+            Initials = "LM";
+            Name = "Luke";
+            Message = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam";
+            ProfilePictureRGB = "ecf6fb";
+        }
+    }
+}
